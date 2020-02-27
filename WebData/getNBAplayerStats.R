c94bc82ea6d3df3a72ed5260ad2e2ecf00ea0531
Tyler Ransom <tyleransom@users.noreply.github.com>
2020-02-27 08:55:45 -0600

    add NBA salaries and stats for optimization exmpl

---
+++ WebData/getNBAplayerStats.R
@@ -0,0 +1,13 @@
+library(tidyverse, warn.conflict=FALSE)
+library(magrittr)
+library(nbastatR)
+
+players <- seasons_players(2020, nest_data = F, return_message=T)
+
+salaries <- hoopshype_salaries()
+
+stats <- metrics_leaders(seasons=2020, metric = "pts", modes = "PerGame")
+
+final <- left_join(salaries,stats, by=("namePlayer")) %>% drop_na(amountContract,fgm) %>% filter(slugSeason.x=="2019-20")
+
+final %>% select(namePlayer,slugTeam,amountContract,minutes,ftm,fta,fgm,fga,fg3m,pts,treb,ast,tov,eff) %>% write.csv("playerSalaryStats.csv")
